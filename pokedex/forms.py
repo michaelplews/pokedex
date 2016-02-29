d963ac7264dd30540f6478ae71446e388985a1c6
michaelplews <michael.plews@gmail.com>
2016-02-29 17:18:43 -0600

    Added database cooperation for users

--- pokedex/forms.py
+++ pokedex/forms.py
@@ -21,9 +21,9 @@ class SampleForm(forms.ModelForm):
 	name = forms.CharField(max_length=200, required=True)
 	formula = forms.CharField(max_length=50, required=False)
 
-	experiment_variable = forms.FloatField(label='Variable')
-	experiment_time = forms.FloatField(label='Time (h)')
-	experiment_equation = forms.CharField(label='Stoichiometric Equation', max_length=200)
+	experiment_variable = forms.FloatField(label='Variable', required=True)
+	experiment_time = forms.FloatField(label='Time (h)', required=True)
+	experiment_equation = forms.CharField(label='Stoichiometric Equation', max_length=200, required=False)
 
 
 	start_date = forms.DateField(widget=DateInput(),
@@ -31,13 +31,13 @@ class SampleForm(forms.ModelForm):
 	end_date = forms.DateField(widget=DateInput(),
                                   required=False)
 
-	analysis_XRD = forms.BooleanField(label='XRD')
-	analysis_EC = forms.BooleanField(label='EChem')
-	analysis_TEM = forms.BooleanField(label='TEM')
-	analysis_TGA = forms.BooleanField(label='TGA')
-	analysis_XAS = forms.BooleanField(label='XAS')
+	analysis_XRD = forms.BooleanField(label='XRD', required=False)
+	analysis_EC = forms.BooleanField(label='EChem', required=False)
+	analysis_TEM = forms.BooleanField(label='TEM', required=False)
+	analysis_TGA = forms.BooleanField(label='TGA', required=False)
+	analysis_XAS = forms.BooleanField(label='XAS', required=False)
 
 	class Meta:
 		model = models.Sample
 		fields = ['sample_number', 'name', 'formula', 'experiment_medium', 'experiment_atmosphere',
-					'experiment_variable', 'experiment_time', 'experiment_equation', 'start_date', 						'end_date', 'analysis_XRD', 'analysis_EC', 'analysis_TEM', 'analysis_TGA', 						'analysis_XAS']
+					'experiment_variable', 'experiment_time', 'experiment_equation', 'start_date', 						'end_date', 'analysis_XRD', 'analysis_EC', 'analysis_TEM', 'analysis_TGA', 						'analysis_XAS', 'associated_project']
